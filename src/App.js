94df2fc8a399439dce6df8173860afb5e96b9e22
haru <hrtkszk@gmail.com>
2021-10-31 13:52:03 +0900

    second try

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
-import { DataStore } from '@aws-amplify/datastore';
-import { Dynamodbtest01 } from './models';
+// import { DataStore } from '@aws-amplify/datastore';
+// import { Dynamodbtest01 } from './models';
 
 function App() {
-    DataStore.save(
-      new Dynamodbtest01({
-      "timestamp": 1023123,
-      "datetime": "1970-01-01T12:30:23.999Z"
-    })
-    );
+    // DataStore.save(
+    //   new Dynamodbtest01({
+    //   "timestamp": 1023123,
+    //   "datetime": "1970-01-01T12:30:23.999Z"
+    // })
+    // );
+
+    var docClient = new AWS.DynamoDB.DocumentClient();
+    var params = {
+        TableName: tablename,
+        Item:{
+          "timestamp": 1023123,
+          "datetime": "1970-01-01T12:30:23.999Z"
+            //  identityId: identityId, // ★prime partition key
+            //  email: _self.state.email,
+            //  username: _self.state.username,
+            //  filename: filepath,
+            //  thumbnail: thumbnail,
+            //  type: fileType,
+            //  title: title,
+            //  story: story,
+            //  imageOverwrite: _self.state.imageOverwrite,
+            //  mapUse: _self.state.mapUse,
+            //  position: _self.state.position,
+            //  uploadTime: uploadTime, // ★prime & secondary sort key
+            //  uploadDate: uploadDate,
+            //  partitionYear: partitionYear, //★secondary partition key
+            //  refCounter: 0
+        }
+    };
+    docClient.put(params, function(err, data) {
+        if(err) {
+            console.log("Err: table put :" +err);
+        } else {
+            console.log("Success: table put ok");
+        }
+    });
   return (
     <div className="App">
       <header className="App-header">
